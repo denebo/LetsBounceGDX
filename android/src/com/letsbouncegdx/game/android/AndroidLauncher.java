a9f70db1dfdb7f5ba8bcc9569e61d20ce6bced39
U-Nuggz-TNK\Deneb <Deneb@Nuggz-TNK.(none)>
2014-05-02 17:22:26 -0500

    The beginning

---
+++ android/src/com/letsbouncegdx/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.letsbouncegdx.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.letsbouncegdx.game.GameClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameClass(), config);
+	}
+}
